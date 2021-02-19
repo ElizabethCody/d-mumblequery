8caa18dd56d503688a07fccae89d8a87ebff57a2
Maxwell Cody <maxwell@cody.sh>
2021-02-18 19:14:02 -0600

    Separate MumblePing struct.

--- source/app.d
+++ source/app.d
@@ -1,11 +1,11 @@
 import std.stdio;
 import std.socket;
+import std.conv : to;
+import std.regex : regex, matchFirst;
+import std.string : strip;
+import mumbleping;
 
 void main(string[] args) @safe {
-  import std.conv : to;
-  import std.regex : regex, matchFirst;
-  import std.string : strip;
-
   if(args.length == 1) {
     writefln("Usage: %s [<host[:port]>...]", args[0]);
   } else {
@@ -45,32 +45,5 @@ outer:
 
       writeln();
     }
-  }
-}
-
-struct MumblePing {
-  import std.random : uniform;
-  import std.datetime : dur;
-  import std.bitmanip : bigEndianToNative, nativeToBigEndian;
-
-  immutable ubyte[4] serverVersion;
-  immutable uint users, slots, bandwidth;
-
-  static MumblePing query(Address address) @safe {
-    auto socket = new UdpSocket(address.addressFamily);
-    auto id = uniform!ulong;
-    socket.setOption(SocketOptionLevel.SOCKET, SocketOption.RCVTIMEO, dur!"seconds"(15));
-    socket.sendTo(nativeToBigEndian!uint(0) ~ nativeToBigEndian(id), address);
-
-    ubyte[24] response;
-    socket.receiveFrom(response, address);
-    socket.close();
-
-    assert(id == bigEndianToNative!ulong(response[4..12]), "packet id mismatch");
-    auto users = bigEndianToNative!uint(response[12..16]);
-    auto slots = bigEndianToNative!uint(response[16..20]);
-    auto bandwidth = bigEndianToNative!uint(response[20..24]);
-
-    return MumblePing(response[0..4], users, slots, bandwidth);
   }
 }
---
+++ source/mumbleping.d
@@ -0,0 +1,27 @@
+import std.socket;
+import std.random : uniform;
+import std.datetime : dur;
+import std.bitmanip : bigEndianToNative, nativeToBigEndian;
+
+struct MumblePing {
+  immutable ubyte[4] serverVersion;
+  immutable uint users, slots, bandwidth;
+
+  static MumblePing query(Address address) @safe {
+    auto socket = new UdpSocket(address.addressFamily);
+    auto id = uniform!ulong;
+    socket.setOption(SocketOptionLevel.SOCKET, SocketOption.RCVTIMEO, dur!"seconds"(15));
+    socket.sendTo(nativeToBigEndian!uint(0) ~ nativeToBigEndian(id), address);
+
+    ubyte[24] response;
+    socket.receiveFrom(response, address);
+    socket.close();
+
+    assert(id == bigEndianToNative!ulong(response[4..12]), "packet id mismatch");
+    auto users = bigEndianToNative!uint(response[12..16]);
+    auto slots = bigEndianToNative!uint(response[16..20]);
+    auto bandwidth = bigEndianToNative!uint(response[20..24]);
+
+    return MumblePing(response[0..4], users, slots, bandwidth);
+  }
+}
